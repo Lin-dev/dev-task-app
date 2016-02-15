37e9b2afd99712af07f6ca46ffeaaafb78893d6f
bklynate <nathanielcfa@gmail.com>
2016-02-14 19:58:04 -0500

    Add tests for description presence

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -49,4 +49,22 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("Hello World! first rspec test")
   end
+
+  it "displays an error when the todo list has no description" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Hello World!"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Hello World!")
+  end
 end
